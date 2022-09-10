6bdd1ae56a7bec78e3faaf08b8b8557b70d2ed41
= <=>
2022-09-09 20:27:44 -0400

    I'm gonna need object oriented programmers anonymous after this one

---
+++ 109.4.variablesPlsHelp.java
@@ -0,0 +1,27 @@
+// Before you ask: no, you can't use arrays. or loops. no, I'm not doing okay.
+import java.util.Scanner;
+
+class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter mark 1:");
+        double mark1 = scanner.nextDouble();
+        System.out.println("Marks: " + mark1);
+        System.out.println("Enter mark 2:");
+        double mark2 = scanner.nextDouble();
+        double total = mark1 + mark2;
+        System.out.println("Marks: " + mark1 + ", " + mark2);
+        System.out.println("Mark total is " + total);
+        System.out.println("Enter mark 3:");
+        double mark3 = scanner.nextDouble();
+        System.out.println("Marks: " + mark1 + ", " + mark2 + ", " + mark3);
+        total = mark1 + mark2 + mark3;
+        System.out.println("Mark total is " + total);
+        System.out.println("Enter mark 4:");
+        double mark4 = scanner.nextDouble();
+        System.out.println("Marks: " + mark1 + ", " + mark2 + ", " + mark3 + ", " + mark4);
+        total = mark1 + mark2 + mark3 + mark4;
+        System.out.println("Mark total is " + total);
+        System.out.println("The average is " + total/4);
+    }
+}
