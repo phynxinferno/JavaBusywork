c24821ff3ad831f68e0237af8525fb381677331d
= <=>
2022-09-11 20:38:59 -0400

    ONE LEFT AND I'M OUT FREE GONE DONE CAN PROGRAM IN DART BECAUSE IT SEEMS COOL PROBABLY NOT GONNA DO OOP AGAIN IN THE NEXT FEW DAYS

--- 111.moreJavaLangMath.java
+++ 111.moreJavaLangMath.java
@@ -6,6 +6,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Please enter a positive, 4-digit number (1111 to 9999): ");
         int integer = inStream.nextInt();
-        System.out.println("The digits of " + integer + " are " + integer/1000 + ", " + (integer % 1000)/100 + ", " + ((integer % 1000) % 100)/10 + ", and " + (((integer % 1000) % 100) % 10) + ".");
+        System.out.println("The digits of " + integer + " are " + integer/1000 + ", " + (integer % 1000)/100 + ", " + ((integer % 1000) % 100)/10 + ", and " + (((integer % 1000) % 100)% 10) + ".");
     }
 }
